2d97fee8703d169c7d13bd034e698d933dcf79d7
bmd1307 <bmd1307@g.rit.edu>
2018-01-25 12:15:59 -0500

    Added comments

--- sne_outskirts.py
+++ sne_outskirts.py
@@ -6,6 +6,25 @@ import matplotlib.pyplot as plt
 
 class Supernova:
     """Representation of a Supernova"""
+    """Contains data from Table 4 from Leaman et al. (2010)"""
+    """A Supernova object contains:"""
+    """ sn_name                 - The supernova's name (ex. 1998S) """
+    """ sn_offset_given         - The string representation of the offset of the SN from its host's nucleus in arcsecs (ex. '16.0W 4.5N') """
+    """ sn_magnitude            - The supernova's magnitude """
+    """ sn_ra_deg               - The supernova's right ascension in degrees (converted from hms in the file)"""
+    """ sn_dec_deg              - The supernova's declination in degrees (converted from dms in the file)"""
+    """ sn_type                 - The type of the supernova (Ia, Ib, ...)"""
+    """ galaxy_name             - The name of the host galaxy (underscore format, ex. 'NGC_3877')"""
+    """ galaxy_ra_deg           - The right ascension of the host galaxy in degrees (converted from hours format in the file)"""
+    """ galaxy_dec_deg          - The declination of the host galaxy in degrees"""
+    """ galaxy_dist_mpc         - The distance to the host galaxy in Mpc"""
+    """ galaxy_hubble_type      - The hubble type of host galaxy (coded as an integer, see Leaman et al.)"""
+    """ galaxy_maj_axis_min     - The major axis length of the host galaxy in arcmin"""
+    """ galaxy_min_axis_min     - The minor axis length of the host galaxy in arcmin"""
+    """ galaxy_incl_deg         - The inclination angle of the host galaxy"""
+    """ galaxy_position_angle   - The posiiton angle of the host galaxy"""
+
+    # Creates a Supernova object from a line from the supernova file
     def __init__(self, file_line):
         line_vals = file_line.split()
         
@@ -20,7 +39,8 @@ class Supernova:
         sn_ra_sec               = float(line_vals[14])
         # converts the hms format of the RA in the file to degrees
         self.sn_ra_deg          = (sn_ra_hrs + (sn_ra_min / 60.0) + (sn_ra_sec / 3600.0)) * (360.0 / 24.0)
-        
+
+        # converts the dms format of the dec to degrees
         sn_dec_deg              = float(line_vals[15])
         sn_dec_min              = float(line_vals[16])
         sn_dec_sec              = float(line_vals[17])
@@ -49,28 +69,40 @@ class Supernova:
 
         self.galaxy_pa_deg      = float(line_vals[37])
 
-        
+    # calculates the offset of the SN from the host galaxy by subtracting their coordinates
+    # returns a string of the offset in arcsec (ex. '16.0W 4.5N')
     def offset(self):
+        # get the ra and dec differences for the SN
         delta_ra_sec = (self.sn_ra_deg - self.galaxy_ra_deg) * 3600
         delta_dec_sec = (self.sn_dec_deg - self.galaxy_dec_deg) * 3600
 
+        # round the ra to one decimal point of one arcsec
         offset_ra = str(abs(round(delta_ra_sec, 1)))
+        # append W if the ra offset is negative, E if positive
         if delta_ra_sec < 0:
             offset_ra = offset_ra + 'W'
         else:
             offset_ra = offset_ra + 'E'
 
+        # round the dec to one decimal point of one arcsec
+        # append S if the ra offset is negative, N if positive
         offset_dec = str(abs(round(delta_dec_sec, 1)))
         if delta_dec_sec < 0:
             offset_dec = offset_dec + 'S'
         else:
             offset_dec = offset_dec + 'N'
 
+        # assemble the parts and return the string
         return offset_ra + ' ' + offset_dec
 
+    # calculate the distance ratio (Rsn / Rgal)
+    # offset_type sets the calculation mode:
+    #   "calc" - the offset of the SN is calculated by subtracting the SN ra and dec from the galaxy's ra and dec
+    #   "read" - uses the offset as provided in the file (not always the same as the offset in "calc" mode)
     def distance_ratio(self, offset_type):
         axis_ratio = self.galaxy_maj_axis_min / self.galaxy_min_axis_min
 
+        # gets the offset in arcsec
         offset_x = None
         offset_y = None
         if offset_type == "calc":
@@ -83,7 +115,8 @@ class Supernova:
             offset_y = offset_vals[1]
         else:
             print('invalid offset type in distance_ratio')
-        
+
+        # calculates the offset vector after it has been rotated so the galaxy's major axis is vertical, then dilates the x component by the axis ratio
         adjusted_offset_x = axis_ratio * \
                             (offset_x * cosd(self.galaxy_pa_deg) - \
                              offset_y * sind(self.galaxy_pa_deg))
@@ -93,8 +126,13 @@ class Supernova:
 
         maj_axis_sec = 60 * self.galaxy_maj_axis_min
 
+        # returns the length of the adjusted offset vector (hypot(Ox, Oy)) divided by the galaxy's radius (1/2 the major axis)
         return math.hypot(adjusted_offset_x, adjusted_offset_y) / (0.5 * maj_axis_sec)
 
+    # Determines the general SN class of this supernova
+    # Ex. a SN type of 'IatXF' is converted to 'Ia'
+    # The main classes of SNe are: '?', 'Ia', 'Ib', 'Ibc', 'Ic', and 'II'
+    # anything after these main classes is ignored in the return string
     def sn_type_class(self):
         if self.sn_type[0] == '?':
         #if '?' in self.sn_type:
@@ -111,24 +149,30 @@ class Supernova:
             else:
                 return 'Ib'
 
+# a helper function: returns the sin of theta where theta is degrees (math.sin takes radians)
 def sind(theta):
     return math.sin(math.radians(theta))
 
+# a helper function: returns the cos of theta where theta is degrees (math.cos takes radians)
 def cosd(theta):
     return math.cos(math.radians(theta))
 
+# returns the pythagorean distance between two strings in offset format ('16.0W 4.5N')
 def offset_distance(os1, os2):
     os1_c = offset_to_coords(os1)
     os2_c = offset_to_coords(os2)
 
     return math.hypot(os1_c[0] - os2_c[0], os1_c[1] - os2_c[1])
 
+# returns a tuple of the difference between two offset strings ('16.0W 4.5N')
 def offset_distance_manhattan(os1, os2):
     os1_c = offset_to_coords(os1)
     os2_c = offset_to_coords(os2)
 
     return (round(os1_c[0] - os2_c[0], 1), round(os1_c[1] - os2_c[1], 1))
 
+# converts an offset string into a tuple in arcsec
+# ex. '16.0W 4.5N' -> (-16.0, 4.5)
 def offset_to_coords(os):
     os_vals = os.split()
         
@@ -142,6 +186,7 @@ def offset_to_coords(os):
 
     return (os_x, os_y)
 
+# Reads each line in the file (file_name), creates a supernova object from it, and adds each supernova to the given array (sn_list)
 def parse_sne_file(sn_list, file_name):
     print('Parsing file ' + file_name)
 
@@ -151,6 +196,8 @@ def parse_sne_file(sn_list, file_name):
 
     print('Parsed ' + str(len(sn_list)) + ' supernovae')
 
+# normalizes each value in a list to 0.0 - 1.0
+# ex [1, 2, 3, 5] -> [0, 0.25, 0.5, 1.0]
 def normalize(arr):
 
     max_val = max(arr)
@@ -158,61 +205,74 @@ def normalize(arr):
 
     return [(n - min_val) / (max_val - min_val) for n in arr]
 
-def __main__():
-    print(" *** sne_outskirts.py *** ")
+# Recreates the plot in Figure 8 of Leaman et al., the cumulative distributions of the SN types (Ia, Ib, Ibc, Ic, II)
+def plot_sne_cumulative():
 
+    # parses the full sample of supernovae
     sne_list = []
-
     parse_sne_file(sne_list, 'sn-full.txt')
 
-    num_all_sne = len(sne_list)
-    
     # remove the galaxies with a error in the position angle (-99.999)
+    num_all_sne = len(sne_list)
     sne_list = [sn for sn in sne_list if sn.galaxy_pa_deg >= 0.0]
-
     print('Removed', str(num_all_sne - len(sne_list)), ' SNe with host galaxies that had bad position angles')
 
+    # create a new list for each type of supernovae
+
+    # Ia list
     sne_Ia = [sn for sn in sne_list if sn.sn_type_class() == 'Ia']
     print('Found', len(sne_Ia), 'SNe of type Ia')
 
+    # Ib list
     sne_Ib = [sn for sn in sne_list if sn.sn_type_class() == 'Ib']
     print('Found', len(sne_Ib), 'SNe of type Ib')
 
-    # The Ibc plot in this graph is the Ibs, the Ics and Ibcs
+    # Ibc list (is actually the combination of Ib, Ibc and Ic SNe)
     sne_Ibc = [sn for sn in sne_list if (sn.sn_type_class() == 'Ibc' \
                                     or  sn.sn_type_class() == 'Ib' \
                                     or  sn.sn_type_class() == 'Ic')]
     print('Found', len(sne_Ibc), 'SNe of type Ib, Ic or Ibc')
 
+    # Ic list
     sne_Ic = [sn for sn in sne_list if sn.sn_type_class() == 'Ic']
     print('Found', len(sne_Ic), 'SNe of type Ic')
 
+    # II list
     sne_II = [sn for sn in sne_list if sn.sn_type_class() == 'II']
     print('Found', len(sne_II), 'SNe of type II')
 
+    # uses the given offset of the SN in the file to calculate the distance ratio
     offset_method = "read"
 
+    # plots the Ia line (blue dot dash)
     plot_Ia, = plt.step(sorted([sn.distance_ratio(offset_method) for sn in sne_Ia]),\
              normalize(list(range(0, len(sne_Ia)))),\
              color = 'b', linestyle = '-.', linewidth = 2, label = 'Ia')
 
+    # plots the Ib line (magenta dot dash)
     plot_Ib, = plt.step(sorted([sn.distance_ratio(offset_method) for sn in sne_Ib]),\
              normalize(list(range(0, len(sne_Ib)))),\
              color = 'm', linestyle = '-.', linewidth = 2, label = 'Ib')
 
+    # plots the Ibc line (black dashed)
     plot_Ibc, = plt.step(sorted([sn.distance_ratio(offset_method) for sn in sne_Ibc]),\
              normalize(list(range(0, len(sne_Ibc)))),\
              color = 'k', linestyle = '--', linewidth = 2, label = 'Ibc')
-    
+
+    # plots the Ic line (gray solid)
     plot_Ic, = plt.step(sorted([sn.distance_ratio(offset_method) for sn in sne_Ic]),\
              normalize(list(range(0, len(sne_Ic)))),\
              color = '#777777', linestyle = '-', linewidth = 2, label = 'Ic')
 
+    # plots the II line (red solid)
     plot_II, = plt.step(sorted([sn.distance_ratio(offset_method) for sn in sne_II]),\
              normalize(list(range(0, len(sne_II)))),\
              color = 'r', linestyle = '-', linewidth = 2, label = 'II')
 
+    # x from 0.05 - 1.4 (distance ratio)
     plt.xlim(0.05, 1.4)
+    
+    # y from 0 - 1.1 ( cumulative frequency
     plt.ylim(0, 1.1)
 
     plt.legend(handles = [plot_II, plot_Ia, plot_Ib, plot_Ibc, plot_Ic], loc=4)
@@ -221,7 +281,13 @@ def __main__():
     plt.ylabel('Cumulative Fraction')
     plt.title('Duplicated Figure 8')
 
+    # displays the plot
     plt.show()
+
+def __main__():
+    print(" *** sne_outskirts.py *** ")
+
+    plot_sne_cumulative()
     
 __main__()
     
